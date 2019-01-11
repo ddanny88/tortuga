e6a90b66cc4cfe112ee38651b079d5a92815b3fb
Daniel Dominguez <ddominguez20.dd@gmail.com>
2019-01-11 13:26:20 -0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const { json } = require("body-parser");
+const session = require("express-session");
+const massive = require("massive");
+// const { check } = require("./middlewares/checkSession");
+
+
+const app = express();
+app.use(json());
+
+//connecting to the databse:
+massive(process.env.CONNECTION_STRING).then(db => {
+  app.set("db", db);
+  console.log("DATABASE CONNECTED.");
+});
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    }
+  })
+);
+// checks if the user in on session, if not, a session is created and the user is added to the session.
+// app.use(check);
+
+
+const PORT = 3001
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
